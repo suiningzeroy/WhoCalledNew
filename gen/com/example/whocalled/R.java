3a707026c68ab498dc62ad1900b1431cbee7ce62
zeroy <todezou18@163.com>
2013-08-25 11:23:32 +0800

    moidfied the layout and add permission

--- gen/com/example/whocalled/R.java
+++ gen/com/example/whocalled/R.java
@@ -24,10 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int WhoCalled=0x7f080000;
+        public static final int WhoCalledList=0x7f080001;
+        public static final int action_settings=0x7f080009;
+        public static final int average_duration=0x7f080008;
+        public static final int counts=0x7f080007;
+        public static final int duration=0x7f080006;
+        public static final int l=0x7f080002;
+        public static final int name=0x7f080004;
+        public static final int number=0x7f080005;
+        public static final int userImage=0x7f080003;
     }
     public static final class layout {
         public static final int activity_who_called=0x7f030000;
+        public static final int list_item=0x7f030001;
     }
     public static final class menu {
         public static final int who_called=0x7f070000;
